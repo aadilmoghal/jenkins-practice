21a446448f2cb775b7cc87eb81e4b763c067a8ef
aadilmoghal <78487765+aadilmoghal@users.noreply.github.com>
2021-04-01 14:45:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 eksctl create cluster --name SQLCluster --region eu-west-2 --nodegroup-name DemoNodes --nodes 2 --nodes-min 2 --nodes-max 3 --node-type t3.medium --with-oidc --ssh-access --ssh-public-key aws-key --managed
-
-kubectl apply -f mysql.yaml
