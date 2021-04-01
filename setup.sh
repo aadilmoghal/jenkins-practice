efaf97567b1f09b809937106e1da2cc40f569b9e
aadilmoghal <78487765+aadilmoghal@users.noreply.github.com>
2021-04-01 14:13:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd Jenkins-k8s 
-
-eksctl create cluster --name DemoCluster --region eu-west-2 --nodegroup-name DemoNodes --nodes 2 --nodes-min 2 --nodes-max 3 --node-type t3.medium --with-oidc --ssh-access --ssh-public-key terraformKey --managed
+eksctl create cluster --name SQLCluster --region eu-west-2 --nodegroup-name DemoNodes --nodes 2 --nodes-min 2 --nodes-max 3 --node-type t3.medium --with-oidc --ssh-access --ssh-public-key aws-key --managed
 
 kubectl apply -f mysql.yaml
