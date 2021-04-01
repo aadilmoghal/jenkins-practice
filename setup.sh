df3c6bb5f3dd48414495c54c9a599e13df3b776b
aadilmoghal <78487765+aadilmoghal@users.noreply.github.com>
2021-04-01 14:03:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
 echo 'launching cluster'
-eksctl create cluster \
---name JenkinsCluster \
---region eu-west-2 \
---nodegroup-name DemoNodes \
---nodes 2 \
---nodes-min 2 \
---nodes-max 3 \
---node-type t2.micro \
---with-oidc \
---ssh-access \
---ssh-public-key aws-key \
---managed
-EOF
+
+eksctl create cluster --name MySQLClusterTask --region eu-west-2 --nodegroup-name SQLNodes--nodes 2 --nodes-min 2 --nodes-max 3 --node-type t3.medium --with-oidc --ssh-access --ssh-public-key aws-key --managed
